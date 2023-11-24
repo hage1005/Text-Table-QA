75c015c93df86b205a92179cbbd883efc70b8b42
Liwen <dominicslw@gmail.com>
2023-11-24 13:15:58 -0500

    Modify test path

--- read_test.sh
+++ read_test.sh
@@ -4,7 +4,7 @@ python read.py \
     --load_dir read1 \
     --output_dir read2 \
     --dev_data_path './Data/HybridQA/test.row.json' \
-    --predict_save_path './Data/HybridQA/test_answers_local.json'\
+    --predict_save_path './Data/HybridQA/test_answers.json'\
     --is_train 0 \
     --is_test 1 
     
